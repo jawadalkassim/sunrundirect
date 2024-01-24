5d905e100253cac738820d0127bbbd78f5755cf5
jawadalkassim <jawadalkassim@gmail.com>
2024-01-24 03:43:50 +0300

    remove ghl

--- v4/index.php
+++ v4/index.php
@@ -17083,24 +17083,10 @@ console.log(payout);
 
                                                 );
                                             } else {
-                                                if (res.code === 200) {
-                                                    const posted = res?.body?.posted || false;
-                                                    if (posted) {
-                                                        pollForSuccess(res.body.conversion_id, success);
-                                                    } else {
-                                                        measureSubmissionDuration.capture('complete');
-                                                        success();
-                                                    }
-                                                } else {
-
-                                                    $("#loader").hide();
-                                                    $('#form_box').show();
-                                                    const question = res?.body?.question || 10;
-                                                    const fieldName = getFieldName(question);
-                                                    $('.btn-next').removeAttr('disabled');
-                                                    progressFieldsetSteps(question, fieldName);
-                                                    $("input, select").removeAttr('disabled', 'disabled');
-                                                }
+                                                
+                                                        pollForSuccess(1, true);
+                                                    
+                                               
                                             }
                                         });
 
