3447df10fae43b6bd55603b6e1303d40715479c5
jawadalkassim <jawadalkassim@gmail.com>
2024-01-24 03:40:13 +0300

    remove ghl

--- v4/index.php
+++ v4/index.php
@@ -17076,25 +17076,9 @@ console.log(payout);
 
                                                 fetch(`https://www.ecomfyl.com/?nid=1006&oid=1274&affid=728&amount=${payout}`).then(
 
-                                                    () => {
-                                                        if (res.code === 200) {
-                                                            const posted = res?.body?.posted || false;
-                                                            if (posted) {
-                                                                pollForSuccess(res.body.conversion_id, success);
-                                                            } else {
-                                                                measureSubmissionDuration.capture('complete');
-                                                                success();
-                                                            }
-                                                        } else {
+                                                    (res) => {
+                                                        pollForSuccess(1, true);
 
-                                                            $("#loader").hide();
-                                                            $('#form_box').show();
-                                                            const question = res?.body?.question || 10;
-                                                            const fieldName = getFieldName(question);
-                                                            $('.btn-next').removeAttr('disabled');
-                                                            progressFieldsetSteps(question, fieldName);
-                                                            $("input, select").removeAttr('disabled', 'disabled');
-                                                        }
                                                     }
 
                                                 );
